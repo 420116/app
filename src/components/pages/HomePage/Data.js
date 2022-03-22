85cbd7a4fd198c8100b58bcd49708ed959ae278a
Your Name <ashish.baghel@sap.com>
2022-03-22 10:06:42 +0530

    Spelling changes

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -4,7 +4,7 @@ export const homeObjOne = {
     lightTextDesc:true,
     topLine:'Call for offers',
     headline:'Experience the Best Beauty Make Over Services',
-    description:'Our beauty salon treatments will help you relax after a long and stressful day. At Bhayvya Beauty Makeover, you can pamper yourself and enjoy the benefits of professional and affordable beauty care. Enjoy your life with our high-quality beauty services.',
+    description:'Our beauty salon treatments will help you relax after a long and stressful day. At Bhavya Beauty Makeover, you can pamper yourself and enjoy the benefits of professional and affordable beauty care. Enjoy your life with our high-quality beauty services.',
     buttonLabel:'Packages',
     imgStart:'',
     img:'../../images/svg-1.svg',
